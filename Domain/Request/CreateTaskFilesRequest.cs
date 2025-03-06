b39676b593bc8055b9770e0931280a991ac08f15
Abner Fernandes <Abner_70@hotmail.com>
2025-03-06 09:52:21 -0300

    Id Removal - Task Files

--- Domain/Request/CreateTaskFilesRequest.cs
+++ Domain/Request/CreateTaskFilesRequest.cs
@@ -2,7 +2,6 @@
 {
     public class CreateTaskFilesRequest
     {
-        public int Id { get; set; }
         public int ProjectTaskId { get; set; }
         public string FileTitle { get; set; }
         public string FileURL { get; set; }
